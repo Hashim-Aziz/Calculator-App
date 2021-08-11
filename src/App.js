26b1cba3a8fbd706bc3b3ed1b560da6e74b3dfa6
HashimAziz <hashimkayani97@gmail.com>
2021-08-11 13:09:51 +0500

    project layout comleted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from "react";
+import Buttons from "./Buttons";
+
 function App() {
   return (
-    <div>
-      <h2>Hello World</h2>
-    </div>
+    <section>
+      <div className="calculator">
+        <div className="center">
+          <input type="text" className="formula-display" />
+          <input type="text" className="input-display " value="0" />
+          <Buttons />
+        </div>
+      </div>
+    </section>
   );
 }
 
---
+++ src/Buttons.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Buttons = () => {
+  return (
+    <>
+      <div className="div-bg">
+        <button className="btn mg-lft">History</button>
+      </div>
+      <div className="div-bg">
+        <button className="btn mg-top bg-gray mg-lft">(</button>
+        <button className="btn bg-gray mg-lft">)</button>
+        <button className="btn bg-gray mg-lft">%</button>
+        <button className="btn bg-red mg-lft">AC</button>
+      </div>
+      <div className="div-bg">
+        <button className="btn mg-top mg-lft">7</button>
+        <button className="btn mg-lft">8</button>
+        <button className="btn mg-lft">9</button>
+        <button className="btn bg-gray mg-lft">/</button>
+      </div>
+      <div className="div-bg">
+        <button className="btn mg-top mg-lft">4</button>
+        <button className="btn mg-lft">5</button>
+        <button className="btn mg-lft">6</button>
+        <button className="btn bg-gray mg-lft">*</button>
+      </div>
+      <div className="div-bg">
+        <button className="btn mg-top mg-lft">1</button>
+        <button className="btn mg-lft">2</button>
+        <button className="btn mg-lft">3</button>
+        <button className="btn bg-gray mg-lft">-</button>
+      </div>
+      <div className="div-bg">
+        <button className="btn mg-top mg-lft">0</button>
+        <button className="btn mg-lft">.</button>
+        <button className="btn mg-lft bg-bluish">=</button>
+        <button className="btn bg-gray mg-lft">+</button>
+      </div>
+    </>
+  );
+};
+
+export default Buttons;
